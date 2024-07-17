0023e95cbd66930526be1fd8f08fed134e496f9b
daisukemaki1003 <daisukemaki1003@gmail.com>
2024-07-18 00:23:09 +0900

    chore: Update gulpfile.js to add gulp-watch for file watching

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,48 @@
-import browserSync from 'browser-sync';
-import pkg from 'gulp';
-const { src, lastRun, dest, watch, series, parallel, Gulp } = pkg;
-import gulpif from 'gulp-if';
-import rename from 'gulp-rename';
-import plumber from 'gulp-plumber';
-import notify from 'gulp-notify';
-import imagemin from 'gulp-imagemin';
-import mozjpeg from 'imagemin-mozjpeg';
-import changed from 'gulp-changed';
-import postcss from 'gulp-postcss';
-import mqpacker from 'css-mqpacker';
-import sortCSSmq from 'sort-css-media-queries';
-import dartSass from 'sass';
-import bulkSass from 'gulp-sass-glob-use-forward';
-import gulpSass from 'gulp-sass';
+import browserSync from "browser-sync";
+import pkg from "gulp";
+const { src, dest, series, parallel } = pkg;
+import gulpif from "gulp-if";
+import plumber from "gulp-plumber";
+import notify from "gulp-notify";
+import imagemin from "gulp-imagemin";
+import mozjpeg from "imagemin-mozjpeg";
+import changed from "gulp-changed";
+import postcss from "gulp-postcss";
+import mqpacker from "css-mqpacker";
+import sortCSSmq from "sort-css-media-queries";
+import dartSass from "sass";
 const sass = gulpSass(dartSass);
-import cleanCSS from 'gulp-clean-css';
-import autoprefixer from 'autoprefixer';
-import { rollup } from 'rollup';
-import { deleteAsync } from 'del';
-import path from 'path';
-import minmax from 'postcss-media-minmax';
+import bulkSass from "gulp-sass-glob-use-forward";
+import gulpSass from "gulp-sass";
+import cleanCSS from "gulp-clean-css";
+import autoprefixer from "autoprefixer";
+import { rollup } from "rollup";
+import { deleteAsync } from "del";
+import path from "path";
+import minmax from "postcss-media-minmax";
+import gulpWatch from "gulp-watch";
 
 // Rollupの設定ファイル
-import deploy from './rollup.config.js';
+import deploy from "./rollup.config.js";
+
 
 const paths = {
-    rootDir: 'dist',
+    rootDir: "dist",
     styles: {
-        src: 'theme/css/**/*.scss',
-        dest: 'dist/css',
+        src: "theme/css/**/*.scss",
+        dest: "dist/css",
     },
     scripts: {
-        src: 'theme/js/**/*.{js,jsx,ts,tsx}',
-        dest: 'dist/js',
+        src: "theme/js/**/*.{js,jsx,ts,tsx}",
+        dest: "dist/js",
     },
     images: {
-        src: 'theme/img/**/*.{jpg,jpeg,png,svg,gif}',
-        dest: 'dist/img',
+        src: "theme/img/**/*.{jpg,jpeg,png,svg,gif}",
+        dest: "dist/img",
     },
     static: {
-        src: 'static/**/*.*',
-        dest: 'dist',
+        src: "static/**/*.*",
+        dest: "dist",
     },
     dist: {},
 };
@@ -52,7 +53,7 @@ const serve = (done) => {
     server.init({
         server: {
             baseDir: paths.rootDir,
-            index: 'index.html',
+            index: "index.html",
         },
         online: true,
         open: false,
@@ -68,7 +69,7 @@ const reload = (done) => {
 /*
  * Clean
  */
-const clean = () => deleteAsync(['dist']);
+const clean = () => deleteAsync(["dist"]);
 
 /*
  * SCSS
@@ -77,13 +78,13 @@ const styles = (done) => {
     src(paths.styles.src)
         .pipe(
             plumber({
-                errorHandler: notify.onError('Error: <%= error.message %>'),
+                errorHandler: notify.onError("Error: <%= error.message %>"),
             })
         )
         .pipe(bulkSass())
         .pipe(
             sass.sync({
-                outputStyle: 'expanded',
+                outputStyle: "expanded",
             })
         )
         .pipe(
@@ -95,7 +96,7 @@ const styles = (done) => {
                 }),
             ])
         )
-        .pipe(gulpif(process.env.NODE_ENV === 'production', cleanCSS()))
+        .pipe(gulpif(process.env.NODE_ENV === "production", cleanCSS()))
         .pipe(dest(paths.styles.dest))
         .pipe(server.stream());
     done();
@@ -134,26 +135,37 @@ const Imagemin = (done) => {
     src(paths.images.src)
         .pipe(changed(paths.images.dest))
         .pipe(imagemin([mozjpeg({ quality: 90 })]))
-        .pipe(dest(paths.images.dest))
-    // .pipe(server.stream());
+        .pipe(dest(paths.images.dest));
     done();
 };
 
 const watchFiles = () => {
-    watch(paths.scripts.src, series(scripts, reload));
-    watch(paths.styles.src, styles);
-    watch(paths.images.src, Imagemin);
-    watch(paths.static.src, buildStatic);
+    gulpWatch(paths.scripts.src, series(scripts, reload));
+    gulpWatch(paths.styles.src, styles);
+    gulpWatch(paths.images.src, Imagemin);
+    gulpWatch(paths.static.src, buildStatic);
 
-    watch(paths.styles.src).on('unlink', (filePath) => {
+    gulpWatch(paths.styles.src).on("unlink", (filePath) => {
         if (!/^_/.test(path.parse(filePath).name))
-            deleteAsync(filePath.replace(/src\/css/, paths.styles.dest).replace(/.scss$/, '.css'));
+            deleteAsync(
+                filePath
+                    .replace(/src\/css/, paths.styles.dest)
+                    .replace(/.scss$/, ".css")
+            );
     });
 
-    watch(paths.images.src).on('unlink', (filePath) => deleteAsync(filePath.replace(/dist\/img/, paths.images.dest)));
-    watch(paths.images.src).on('change', (filePath) => deleteAsync(filePath.replace(/dist\/img/, paths.images.dest)));
-    watch(paths.static.src).on('unlink', (filePath) => deleteAsync(filePath.replace(/static/, paths.static.dest)));
-    watch(paths.static.src).on('change', (filePath) => deleteAsync(filePath.replace(/static/, paths.static.dest)));
+    gulpWatch(paths.images.src).on("unlink", (filePath) =>
+        deleteAsync(filePath.replace(/dist\/img/, paths.images.dest))
+    );
+    gulpWatch(paths.images.src).on("change", (filePath) =>
+        deleteAsync(filePath.replace(/dist\/img/, paths.images.dest))
+    );
+    gulpWatch(paths.static.src).on("unlink", (filePath) =>
+        deleteAsync(filePath.replace(/static/, paths.static.dest))
+    );
+    gulpWatch(paths.static.src).on("change", (filePath) =>
+        deleteAsync(filePath.replace(/static/, paths.static.dest))
+    );
 };
 
 export const dist = series(buildStatic, parallel(styles, scripts, Imagemin));
--- theme/functions.php
+++ theme/functions.php
@@ -76,7 +76,7 @@ function custom_theme_enqueue_styles()
 add_action('wp_enqueue_scripts', 'custom_theme_enqueue_styles');
 
 // 画像パスの取得
-function get_image_path($image_name)
+function img($image_name)
 {
     return get_stylesheet_directory_uri() . '/assets/img/' . $image_name;
 }
