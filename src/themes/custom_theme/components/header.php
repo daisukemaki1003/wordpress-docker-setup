759e54c47b9a1bfceb7f5160c388218357f2cf9c
daisukemaki1003 <daisukemaki1003@gmail.com>
2024-10-10 23:39:05 +0900

    Refactor Docker Compose and Gulpfile.js for improved asset management and hot reloading

--- src/themes/custom_theme/components/header.php
+++ src/themes/custom_theme/components/header.php
@@ -29,4 +29,13 @@
     <header class="header_block">
         <h1><?php bloginfo('name'); ?></h1>
         <p><?php bloginfo('description'); ?></p>
+
+        test
+        test
+        test
+
+        test
+        test
+        test
+        test
     </header>
